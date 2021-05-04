0d459b856991c48d140d5348b524d6cbb801a715
SoulFly579 <soulfly579@gmail.com>
2021-05-05 00:39:08 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import './App.css';
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
+import React, { useState } from 'react';
+import { data } from "./data";
+
+function App() {
+    const [url,setUrl] = useState(null);
+
+  return (
+    <div className="App">
+      <header>
+        <div className="header__logo">
+            <img src="https://www.pngkey.com/png/full/22-224230_radio-station-png-clip-art-freeuse-library-radio.png" alt=""/>
+        </div>
+      </header>
+      <div className="container">
+        {data.map((radio)=>{
+        return (           
+                    <div className="radio__container">
+                        <div className="radio">
+                            <div className="radio__logo">
+                                <img src={radio.photo} alt="radioimage" />
+                            </div>
+                            <div className="radio__name">
+                                {radio.name}
+                            </div>
+                            <a className="radio__play" onClick={()=>{ setUrl(radio);}}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-play-fill" viewBox="0 0 16 16">
+                                    <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z"/>
+                                </svg>
+                            </a>
+                        </div>
+                    </div>
+        )
+        })}
+      </div>
+      {url ? <AudioPlayer
+            src={url.link}
+            showJumpControls={false}
+            autoPlay={true}
+            className={"radio__player"}
+        /> : ""}
+        
+    </div>
+  );
+}
+
+export default App;
---
+++ src/data.js
@@ -0,0 +1,10 @@
+export let data = [
+    {name:"Best FM", link:"http://46.20.7.125/listen.pls",photo:"http://www.radyocular.com/wp-content/uploads/2015/11/logo.png"},
+    {name:"İstanbul FM", link:"http://45.32.154.169:9300/;",photo:"https://www.dinle.tv/uploads/post/31840istanbul.png"},
+    {name:"Damar FM", link:"http://yayin.damarfm.com:8080/;",photo:"https://i.pinimg.com/originals/7a/7e/25/7a7e25eb591928d2538110e04fd7a800.png"},
+    {name:"Rock FM", link:"http://stream.radyoodtu.com.tr/rock;",photo:"https://i.pinimg.com/originals/95/b3/e1/95b3e17356f0fbf21ba964bd82f9b5b9.png"},
+    {name:"Slow FM", link:"http://windows.showradyo.com.tr/;",photo:"https://www.slowturk.com.tr/assets/img/logo-slowturk.png"},
+    {name:"Joy FM", link:"https://playerservices.streamtheworld.com/api/livestream-redirect/JOY_FM.mp3",photo:"https://upload.wikimedia.org/wikipedia/tr/6/61/JoyTurk_logosu.png"},
+    {name:"JoyTürk Rock", link:"https://playerservices.streamtheworld.com/api/livestream-redirect/JOYTURK_ROCK.mp3",photo:"https://upload.wikimedia.org/wikipedia/tr/6/61/JoyTurk_logosu.png"},
+    {name:"Pop 90", link:"https://17703.live.streamtheworld.com/POP90.mp3",photo:"https://radyohomestatic.imgix.net/media/station/31/logo_square.png?v=060520050957&auto=format&bg=ff9833"},
+]
